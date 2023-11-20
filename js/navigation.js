16e5fa9a5b7ca77d094503303b607c288fb9414b
Dereku0519 <a5165652@yahoo.com.tw>
2023-11-20 23:43:51 +0800

    tra

--- js/navigation.js
+++ js/navigation.js
@@ -25,7 +25,7 @@ class NavBar {
 		// create mobile "Menu" button
 		const btnShowHide = document.createElement("button");
 		btnShowHide.className = "btn btn-default page__btn-toggle-nav";
-		btnShowHide.innerHTML = "Menu";
+		btnShowHide.innerHTML = "菜單";
 		btnShowHide.onclick = () => {
 			$(btnShowHide).toggleClass("active");
 			$(`.page__nav-hidden-mobile`).toggleClass("block", $(btnShowHide).hasClass("active"));
@@ -40,7 +40,7 @@ class NavBar {
 		addLi(ulRules, "tables.html", "表格");
 		addDivider(ulRules);
 		const ulBooks = addDropdown(ulRules, "出版書籍", true);
-		addLi(ulBooks, "books.html", "View All/Homebrew");
+		addLi(ulBooks, "books.html", "查看所有/自制內容");
 		addDivider(ulBooks);
 		addLi(ulBooks, "book.html", "玩家手冊", {aHash: "PHB", date: "2014"});
 		addLi(ulBooks, "book.html", "怪物圖鑑", {aHash: "MM", date: null});
@@ -49,26 +49,27 @@ class NavBar {
 		addLi(ulBooks, "book.html", "劍灣冒險指南", {aHash: "SCAG", date: "2015"});
 		addLi(ulBooks, "book.html", "瓦羅的怪物指南", {aHash: "VGM", date: "2016"});
 		addLi(ulBooks, "book.html", "姍納薩的萬事指南", {aHash: "XGE", date: "2017"});
-		addLi(ulBooks, "book.html", "魔鄧肯的眾敵卷冊", {aHash: "MTF", date: "2018"});
+		addLi(ulBooks, "book.html", "魔鄧肯的衆敵卷冊", {aHash: "MTF", date: "2018"});
 		addLi(ulBooks, "book.html", "拉尼卡的公會長指南", {aHash: "GGR", date: null});
-		addLi(ulBooks, "book.html", "Acquisitions Incorporated", {aHash: "AI", date: "2019"});
-		addLi(ulBooks, "book.html", "Eberron: Rising from the Last War", {aHash: "ERLW", date: null});
-		addLi(ulBooks, "book.html", "Dungeons & Dragons vs. Rick and Morty: Basic Rules", {aHash: "RMR", date: null});
-		addLi(ulBooks, "book.html", "Explorer's Guide to Wildemount", {aHash: "EGW", date: "2020"});
-		addLi(ulBooks, "book.html", "Mythic Odysseys of Theros", {aHash: "MOT", date: null});
+		addLi(ulBooks, "book.html", "艾奎茲玄有限責任公司", {aHash: "AI", date: "2019"});
+		addLi(ulBooks, "book.html", "艾伯倫：從終末戰爭復甦", {aHash: "ERLW", date: null});
+		addLi(ulBooks, "book.html", "龍與地下城 vs. 瑞克與莫蒂：基礎規則", {aHash: "RMR", date: null});
+		addLi(ulBooks, "book.html", "荒洲探險家指南", {aHash: "EGW", date: "2020"});
+		addLi(ulBooks, "book.html", "塞洛斯的神話奧德賽", {aHash: "MOT", date: null});
 		addLi(ulBooks, "book.html", "塔莎的萬象坩鍋", {aHash: "TCE", date: null});
+		addLi(ulBooks, "book.html", "範·裏希騰的鴉閣指南", {aHash: "VRGR", date: "2021"});
 		addDivider(ulBooks);
-		addLi(ulBooks, "book.html", "Dungeon Master's Screen: Wilderness Kit", {aHash: "ScreenWildernessKit", date: "2020"});
+		addLi(ulBooks, "book.html", "DM帷幕：荒野套件", {aHash: "ScreenWildernessKit", date: "2020"});
 		addDivider(ulBooks);
 		addLi(ulBooks, "book.html", "冒險者聯盟", {aHash: "AL", date: "2016"});
-		addLi(ulBooks, "book.html", "Sage Advice Compendium", {aHash: "SAC", date: "2019"});
+		addLi(ulBooks, "book.html", "智者建言手冊", {aHash: "SAC", date: "2019"});
 
 		const ulPlayers = addDropdown(navBar, "玩家選項");
 		addLi(ulPlayers, "classes.html", "職業");
 		addLi(ulPlayers, "backgrounds.html", "背景");
 		addLi(ulPlayers, "feats.html", "專長");
 		addLi(ulPlayers, "races.html", "種族");
-		addLi(ulPlayers, "charcreationoptions.html", "Other Character Creation Options");
+		addLi(ulPlayers, "charcreationoptions.html", "其他角色創建選項");
 		addLi(ulPlayers, "optionalfeatures.html", "職業能力選項");
 		addDivider(ulPlayers);
 		addLi(ulPlayers, "statgen.html", "屬性生成器");
@@ -77,94 +78,94 @@ class NavBar {
 		addLi(ulPlayers, "names.html", "名稱");
 
 		const ulDms = addDropdown(navBar, "DM工具");
-		addLi(ulDms, "dmscreen.html", "DM屏幕");
+		addLi(ulDms, "dmscreen.html", "DM 帷幕");
 		addDivider(ulDms);
 		const ulAdventures = addDropdown(ulDms, "冒險模組", true);
-		addLi(ulAdventures, "adventures.html", "查看所有/自製內容");
+		addLi(ulAdventures, "adventures.html", "查看所有/自制內容");
 		addDivider(ulAdventures);
-		addLi(ulAdventures, "adventure.html", "Lost Mine of Phandelver", {isSide: true, aHash: "LMoP", date: "2014"});
-		addLi(ulAdventures, "adventure.html", "Hoard of the Dragon Queen", {isSide: true, aHash: "HotDQ", date: null});
-		addLi(ulAdventures, "adventure.html", "Rise of Tiamat", {isSide: true, aHash: "RoT", date: null});
-		addLi(ulAdventures, "adventure.html", "Princes of the Apocalypse", {isSide: true, aHash: "PotA", date: "2015"});
-		addLi(ulAdventures, "adventure.html", "Out of the Abyss", {isSide: true, aHash: "OotA", date: null});
-		addLi(ulAdventures, "adventure.html", "Curse of Strahd", {isSide: true, aHash: "CoS", date: "2016"});
-		addLi(ulAdventures, "adventure.html", "Storm King's Thunder", {isSide: true, aHash: "SKT", date: null});
-		addLi(ulAdventures, "adventure.html", "Tales from the Yawning Portal: The Sunless Citadel", {isSide: true, aHash: "TftYP-TSC", date: "2017"});
-		addLi(ulAdventures, "adventure.html", "Tales from the Yawning Portal: The Forge of Fury", {isSide: true, aHash: "TftYP-TFoF", date: null});
-		addLi(ulAdventures, "adventure.html", "Tales from the Yawning Portal: The Hidden Shrine of Tamoachan", {isSide: true, aHash: "TftYP-THSoT", date: null});
-		addLi(ulAdventures, "adventure.html", "Tales from the Yawning Portal: White Plume Mountain", {isSide: true, aHash: "TftYP-WPM", date: null});
-		addLi(ulAdventures, "adventure.html", "Tales from the Yawning Portal: Dead in Thay", {isSide: true, aHash: "TftYP-DiT", date: null});
-		addLi(ulAdventures, "adventure.html", "Tales from the Yawning Portal: Against the Giants", {isSide: true, aHash: "TftYP-AtG", date: null});
-		addLi(ulAdventures, "adventure.html", "Tales from the Yawning Portal: Tomb of Horrors", {isSide: true, aHash: "TftYP-ToH", date: null});
-		addLi(ulAdventures, "adventure.html", "Tomb of Annihilation", {isSide: true, aHash: "ToA", date: null});
-		addLi(ulAdventures, "adventure.html", "The Tortle Package", {isSide: true, aHash: "TTP", date: null});
-		addLi(ulAdventures, "adventure.html", "Waterdeep: Dragon Heist", {isSide: true, aHash: "WDH", date: "2018"});
-		addLi(ulAdventures, "adventure.html", "Lost Laboratory of Kwalish", {isSide: true, aHash: "LLK", date: null});
-		addLi(ulAdventures, "adventure.html", "Waterdeep: Dungeon of the Mad Mage", {isSide: true, aHash: "WDMM", date: null});
-		addLi(ulAdventures, "adventure.html", "Krenko's Way", {isSide: true, aHash: "KKW", date: null});
-		addLi(ulAdventures, "adventure.html", "Ghosts of Saltmarsh", {isSide: true, aHash: "GoS", date: "2019"});
-		addLi(ulAdventures, "adventure.html", "Hunt for the Thessalhydra", {isSide: true, aHash: "HftT", date: null});
-		addLi(ulAdventures, "adventure.html", "The Orrery of the Wanderer", {isSide: true, aHash: "OoW", date: null});
-		addLi(ulAdventures, "adventure.html", "Essentials Kit: Dragon of Icespire Peak", {isSide: true, aHash: "DIP", date: null});
-		addLi(ulAdventures, "adventure.html", "Essentials Kit: Storm Lord's Wrath", {isSide: true, aHash: "SLW", date: null});
-		addLi(ulAdventures, "adventure.html", "Essentials Kit: Sleeping Dragon's Wake", {isSide: true, aHash: "SDW", date: null});
-		addLi(ulAdventures, "adventure.html", "Essentials Kit: Divine Contention", {isSide: true, aHash: "DC", date: null});
-		addLi(ulAdventures, "adventure.html", "Baldur's Gate: Descent Into Avernus", {isSide: true, aHash: "BGDIA", date: null});
-		addLi(ulAdventures, "adventure.html", "Locathah Rising", {isSide: true, aHash: "LR", date: null});
-		addLi(ulAdventures, "adventure.html", "Eberron: Forgotten Relics", {isSide: true, aHash: "EFR", date: null});
-		addLi(ulAdventures, "adventure.html", "Rick and Morty: Big Rick Energy", {isSide: true, aHash: "RMBRE", date: null});
-		addLi(ulAdventures, "adventure.html", "Infernal Machine Rebuild", {isSide: true, aHash: "IMR", date: null});
-		addLi(ulAdventures, "adventure.html", "Wildemount: Tide of Retribution", {isSide: true, aHash: "ToR", date: "2020"});
-		addLi(ulAdventures, "adventure.html", "Wildemount: Dangerous Designs", {isSide: true, aHash: "DD", date: null});
-		addLi(ulAdventures, "adventure.html", "Wildemount: Frozen Sick", {isSide: true, aHash: "FS", date: null});
-		addLi(ulAdventures, "adventure.html", "Wildemount: Unwelcome Spirits", {isSide: true, aHash: "US", date: null});
-		addLi(ulAdventures, "adventure.html", "Theros: No Silent Secret", {isSide: true, aHash: "MOT-NSS", date: null});
-		addLi(ulAdventures, "adventure.html", "Icewind Dale: Rime of the Frostmaiden", {isSide: true, aHash: "IDRotF", date: null});
-		addLi(ulDms, "cultsboons.html", "異教&惡魔恩惠");
+		addLi(ulAdventures, "adventure.html", "凡戴爾的失落礦坑", {isSide: true, aHash: "LMoP", date: "2014"});
+		addLi(ulAdventures, "adventure.html", "龍後的寶山", {isSide: true, aHash: "HotDQ", date: null});
+		addLi(ulAdventures, "adventure.html", "提亞瑪特的崛起", {isSide: true, aHash: "RoT", date: null});
+		addLi(ulAdventures, "adventure.html", "毀滅親王", {isSide: true, aHash: "PotA", date: "2015"});
+		addLi(ulAdventures, "adventure.html", "逃離深淵", {isSide: true, aHash: "OotA", date: null});
+		addLi(ulAdventures, "adventure.html", "施特拉德的詛咒", {isSide: true, aHash: "CoS", date: "2016"});
+		addLi(ulAdventures, "adventure.html", "風暴君王之雷霆", {isSide: true, aHash: "SKT", date: null});
+		addLi(ulAdventures, "adventure.html", "深水龍門陣：暗無天日", {isSide: true, aHash: "TftYP-TSC", date: "2017"});
+		addLi(ulAdventures, "adventure.html", "深水龍門陣：憤怒熔爐", {isSide: true, aHash: "TftYP-TFoF", date: null});
+		addLi(ulAdventures, "adventure.html", "深水龍門陣：隱祕聖壇", {isSide: true, aHash: "TftYP-THSoT", date: null});
+		addLi(ulAdventures, "adventure.html", "深水龍門陣：白羽山", {isSide: true, aHash: "TftYP-WPM", date: null});
+		addLi(ulAdventures, "adventure.html", "深水龍門陣：死於賽爾", {isSide: true, aHash: "TftYP-DiT", date: null});
+		addLi(ulAdventures, "adventure.html", "深水龍門陣：挑戰巨人", {isSide: true, aHash: "TftYP-AtG", date: null});
+		addLi(ulAdventures, "adventure.html", "深水龍門陣：恐怖墓穴", {isSide: true, aHash: "TftYP-ToH", date: null});
+		addLi(ulAdventures, "adventure.html", "湮滅之墓", {isSide: true, aHash: "ToA", date: null});
+		addLi(ulAdventures, "adventure.html", "龜人擴充包", {isSide: true, aHash: "TTP", date: null});
+		addLi(ulAdventures, "adventure.html", "深水城：龍金飛劫", {isSide: true, aHash: "WDH", date: "2018"});
+		addLi(ulAdventures, "adventure.html", "誇力許的失落實驗室", {isSide: true, aHash: "LLK", date: null});
+		addLi(ulAdventures, "adventure.html", "深水城：瘋法師的地下城", {isSide: true, aHash: "WDMM", date: null});
+		addLi(ulAdventures, "adventure.html", "追捕克侖可", {isSide: true, aHash: "KKW", date: null});
+		addLi(ulAdventures, "adventure.html", "鹽沼幽魂", {isSide: true, aHash: "GoS", date: "2019"});
+		addLi(ulAdventures, "adventure.html", "尋找特薩爾蛇蜥", {isSide: true, aHash: "HftT", date: null});
+		addLi(ulAdventures, "adventure.html", "位面遊蕩儀", {isSide: true, aHash: "OoW", date: null});
+		addLi(ulAdventures, "adventure.html", "起始包：冰塔峯之龍", {isSide: true, aHash: "DIP", date: null});
+		addLi(ulAdventures, "adventure.html", "起始包：風暴領主之怒", {isSide: true, aHash: "SLW", date: null});
+		addLi(ulAdventures, "adventure.html", "起始包：沉睡巨龍醒轉", {isSide: true, aHash: "SDW", date: null});
+		addLi(ulAdventures, "adventure.html", "起始包：神聖的爭奪", {isSide: true, aHash: "DC", date: null});
+		addLi(ulAdventures, "adventure.html", "博德之門：墜入阿弗納斯", {isSide: true, aHash: "BGDIA", date: null});
+		addLi(ulAdventures, "adventure.html", "洛卡魚人崛起", {isSide: true, aHash: "LR", date: null});
+		addLi(ulAdventures, "adventure.html", "艾伯倫：失落的聖物", {isSide: true, aHash: "EFR", date: null});
+		addLi(ulAdventures, "adventure.html", "瑞克與莫蒂：BRE", {isSide: true, aHash: "RMBRE", date: null});
+		addLi(ulAdventures, "adventure.html", "重建煉獄機器", {isSide: true, aHash: "IMR", date: null});
+		addLi(ulAdventures, "adventure.html", "荒洲：復仇之潮", {isSide: true, aHash: "ToR", date: "2020"});
+		addLi(ulAdventures, "adventure.html", "荒洲：危險計劃", {isSide: true, aHash: "DD", date: null});
+		addLi(ulAdventures, "adventure.html", "荒洲：封凍惡疾", {isSide: true, aHash: "FS", date: null});
+		addLi(ulAdventures, "adventure.html", "荒洲：惡客自來", {isSide: true, aHash: "US", date: null});
+		addLi(ulAdventures, "adventure.html", "塞洛斯：無聲之密", {isSide: true, aHash: "MOT-NSS", date: null});
+		addLi(ulAdventures, "adventure.html", "冰風谷：冰霜少女的霧凇", {isSide: true, aHash: "IDRotF", date: null});
+		addLi(ulAdventures, "adventure.html", "燭堡祕辛", {isSide: true, aHash: "CM", date: "2021"});
+		addLi(ulAdventures, "adventure.html", "Ravenloft: The House of Lament", {isSide: true, aHash: "HoL", date: null});
+		addLi(ulDms, "cultsboons.html", "異教&超自然恩惠");
 		addLi(ulDms, "objects.html", "物件");
 		addLi(ulDms, "trapshazards.html", "陷阱&危險");
 		addDivider(ulDms);
-		addLi(ulDms, "crcalculator.html", "CR計算機");
+		addLi(ulDms, "crcalculator.html", "CR 計算器");
 		addLi(ulDms, "encountergen.html", "遭遇生成器");
 		addLi(ulDms, "lootgen.html", "戰利品生成器");
 
-		const ulReferences = addDropdown(navBar, "參照資料");
-		addLi(ulReferences, "actions.html", "Actions");
+		const ulReferences = addDropdown(navBar, "參照數據");
+		addLi(ulReferences, "actions.html", "動作");
 		addLi(ulReferences, "bestiary.html", "怪物圖鑑");
 		addLi(ulReferences, "conditionsdiseases.html", "狀態 & 疾病");
 		addLi(ulReferences, "deities.html", "神祇");
 		addLi(ulReferences, "items.html", "物品");
-		addLi(ulReferences, "languages.html", "Languages");
+		addLi(ulReferences, "languages.html", "語言");
 		addLi(ulReferences, "rewards.html", "其他獎勵");
 		addLi(ulReferences, "psionics.html", "靈能");
 		addLi(ulReferences, "spells.html", "法術");
-		addLi(ulReferences, "vehicles.html", "Vehicles");
+		addLi(ulReferences, "vehicles.html", "載具");
 		addDivider(ulReferences);
-		addLi(ulReferences, "recipes.html", "Recipes");
+		addLi(ulReferences, "recipes.html", "食譜");
 
 		const ulUtils = addDropdown(navBar, "其他功能");
-		addLi(ulUtils, "search.html", "Search");
+		addLi(ulUtils, "search.html", "搜索");
 		addDivider(ulUtils);
 		addLi(ulUtils, "blacklist.html", "內容黑名單");
-		addLi(ulUtils, "makebrew.html", "自製內容生成器");
-		addLi(ulUtils, "managebrew.html", "管理所有自製內容");
+		addLi(ulUtils, "makebrew.html", "自制內容生成器");
+		addLi(ulUtils, "managebrew.html", "管理所有自制內容");
 		addDivider(ulUtils);
-		addLi(ulUtils, "inittrackerplayerview.html", "先攻追蹤器:玩家檢視");
+		addLi(ulUtils, "inittrackerplayerview.html", "先攻追蹤器:玩家查看");
 		addDivider(ulUtils);
 		addLi(ulUtils, "renderdemo.html", "渲染器 Demo");
-		addLi(ulUtils, "makecards.html", "RPG Cards JSON Builder");
-		addLi(ulUtils, "converter.html", "Text Converter");
+		addLi(ulUtils, "makecards.html", "RPG 卡片 JSON 生成器");
+		addLi(ulUtils, "converter.html", "文本轉換器");
 		addDivider(ulUtils);
-		addLi(ulUtils, "plutonium.html", "Plutonium (Foundry Module) Features");
+		addLi(ulUtils, "plutonium.html", "Plutonium (Foundry MOD) 功能");
 		addDivider(ulUtils);
-		addLi(ulUtils, "roll20.html", "Roll20腳本小幫手");
+		addLi(ulUtils, "roll20.html", "Roll20 腳本小幫手");
 		addDivider(ulUtils);
-		addLi(ulUtils, "changelog.html", "Changelog");
-		addLi(ulUtils, `https://wiki.5e.tools/index.php/Page:_${NavBar.getCurrentPage().replace(/.html$/i, "")}`, "Help", {isExternal: true});
+		addLi(ulUtils, "changelog.html", "更新日誌");
+		addLi(ulUtils, `https://wiki.5e.tools/index.php/Page:_${NavBar.getCurrentPage().replace(/.html$/i, "")}`, "幫助", {isExternal: true});
 		addDivider(ulUtils);
-		addLi(ulUtils, "privacy-policy.html", "Privacy Policy");
-
-		addLi(navBar, "donate.html", "Donate", {isRoot: true});
+		addLi(ulUtils, "privacy-policy.html", "隱私政策");
 
 		const ulSettings = addDropdown(navBar, "設置");
 		addButton(
@@ -185,7 +186,7 @@ class NavBar {
 		addButton(
 			ulSettings,
 			{
-				html: styleSwitcher.getActiveWide() === true ? "Disable Wide Mode" : "Enable Wide Mode (Experimental)",
+				html: styleSwitcher.getActiveWide() === true ? "禁用寬幅模式" : "啓用寬幅模式 (實驗功能)",
 				click: (evt) => {
 					evt.preventDefault();
 					styleSwitcher.toggleWide();
@@ -198,13 +199,13 @@ class NavBar {
 		addButton(
 			ulSettings,
 			{
-				html: "儲存狀態至檔案",
+				html: "保存狀態至文件",
 				click: async (evt) => {
 					evt.preventDefault();
 					const sync = StorageUtil.syncGetDump();
 					const async = await StorageUtil.pGetDump();
 					const dump = {sync, async};
-					DataUtil.userDownload("5etools", dump);
+					DataUtil.userDownload("5etools", dump, {fileType: "5etools"});
 				},
 				title: "Save any locally-stored data (loaded homebrew, active blacklists, DM Screen configuration,...) to a file.",
 			},
@@ -212,14 +213,21 @@ class NavBar {
 		addButton(
 			ulSettings,
 			{
-				html: "從檔案讀取狀態",
+				html: "從文件讀取狀態",
 				click: async (evt) => {
 					evt.preventDefault();
-					const dump = await DataUtil.pUserUpload();
+					const jsons = await DataUtil.pUserUpload({expectedFileType: "5etools"});
+					if (!jsons?.length) return;
+					const dump = jsons[0];
 
-					StorageUtil.syncSetFromDump(dump.sync);
-					await StorageUtil.pSetFromDump(dump.async);
-					location.reload();
+					try {
+						StorageUtil.syncSetFromDump(dump.sync);
+						await StorageUtil.pSetFromDump(dump.async);
+						location.reload();
+					} catch (e) {
+						JqueryUtil.doToast({type: "danger", content: `Failed to load state! ${VeCt.STR_SEE_CONSOLE}`});
+						throw e;
+					}
 				},
 				title: "Load previously-saved data (loaded homebrew, active blacklists, DM Screen configuration,...) from a file.",
 			},
@@ -228,7 +236,7 @@ class NavBar {
 		addButton(
 			ulSettings,
 			{
-				html: "Add as App",
+				html: "添加爲 App",
 				click: async (evt) => {
 					evt.preventDefault();
 					try {
@@ -243,7 +251,7 @@ class NavBar {
 		addButton(
 			ulSettings,
 			{
-				html: "Preload Offline Data",
+				html: "預載離線數據",
 				click: async (evt) => {
 					evt.preventDefault();
 
