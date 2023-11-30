52aed4e86176e8f27ad2fe9cc8f19fc849ebfe85
Dereku0519 <a5165652@yahoo.com.tw>
2023-11-30 15:30:07 +0800

    fix

--- js/parser.js
+++ js/parser.js
@@ -209,14 +209,28 @@ Parser.getAbilityModifier = function (abilityScore) {
 	if (modifier >= 0) modifier = `+${modifier}`;
 	return `${modifier}`;
 };
-Parser.getSpeedString = (ent,{isMetric=false, isSkipZeroWalk=false}={})=>{
-    if (ent.speed == null)
-        return "\u2014";
-    const unit = isMetric ? Parser.metric.getMetricUnit({
-        originalUnit: "尺",
-        isShortForm: true
-    }) : "尺";
-    if (typeof it.speed === "object") {
+Parser.getSpeedString = (it) => {
+	if (it.speed == null) return "\u2014";
+
+	function procSpeed (propName) {
+		function addSpeed (s) {
+			stack.push(`${propName === "walk" ? "" : `${Parser.SpeedToDisplay(propName)} `}${getVal(s)} 尺${getCond(s)}`);
+		}
+
+		if (it.speed[propName] || propName === "walk") addSpeed(it.speed[propName] || 0);
+		if (it.speed.alternate && it.speed.alternate[propName]) it.speed.alternate[propName].forEach(addSpeed);
+	}
+
+	function getVal (speedProp) {
+		return speedProp.number != null ? speedProp.number : speedProp;
+	}
+
+	function getCond (speedProp) {
+		return speedProp.condition ? ` ${Renderer.get().render(speedProp.condition)}` : "";
+	}
+
+	const stack = [];
+	if (typeof it.speed === "object") {
 		let joiner = ", ";
 		procSpeed("walk");
 		procSpeed("burrow");
@@ -231,8 +245,7 @@ Parser.getSpeedString = (ent,{isMetric=false, isSkipZeroWalk=false}={})=>{
 	} else {
 		return it.speed + (it.speed === "Varies" ? "" : " 尺");
 	}
-}
-;
+};
 Parser._getSpeedString_addSpeedMode = ({ent, prop, stack, isMetric, isSkipZeroWalk, unit})=>{
     if (ent.speed[prop] || (!isSkipZeroWalk && prop === "walk"))
         Parser._getSpeedString_addSpeed({
